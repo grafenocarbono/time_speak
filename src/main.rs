b15dda632986df9695a3b7e23a9a571836841269
grafenocarbono <grafeno30@gmail.com>
2023-11-20 09:37:35 +0100

    prueba piloto

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+struct Time_{
+    hour: int,
+    minutes:int,
+}
+
+
+fn getTime() -> Time_{
+
+    let mut tempo = Time_{
+        hour: NULL,
+        minutes: NULL,
+    };
+
+    
+
+
+
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn getTime()->
